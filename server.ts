c29b65644d9235eb0a7ba8047858a01418ec9d94
michaelriedemann0-dev <michaelriedemann0@gmail.com>
2026-06-03 14:43:24 +0200

    Fix production distPath using __dirname instead of process.cwd

--- server.ts
+++ server.ts
@@ -306,7 +306,7 @@ async function startServer() {
     app.use(vite.middlewares);
   } else {
     // Production
-    const distPath = path.join(process.cwd(), 'dist');
+    const distPath = path.join(__dirname, 'dist');
     app.use(express.static(distPath));
     app.get('*', (req, res) => {
         res.sendFile(path.join(distPath, 'index.html'));
